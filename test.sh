fdb421e53d1e866cbd6bd84830cabaf4e91ad531
oct0pus <50224007+oct0pus8winner@users.noreply.github.com>
2024-11-21 14:04:13 +0900

    Update test.sh
    update usage
    before: $./test.sh
    after: $./test.sh <source file>

--- test.sh
+++ test.sh
@@ -2,36 +2,39 @@
 
 readonly FILENUM=2
 
-function useage() {
+function usage() {
   cat << _EOT_
-  Usage: $0
-  Description: test wordCount.c file with input_i.txt and output_i.txt
+  Usage: $0 <source_file.c>
+  Description: Test the specified .c file with input_i.txt and output_i.txt
 _EOT_
   exit 1
 }
 
-# Compile Check
-# Wall option : turn on all error
-# Werrror option: treat warning as error
+# Check if a C file is provided as an argument
+if [ $# -ne 1 ]; then
+    usage
+fi
 
-gcc -Wall -Werror -o test_file wordCount.c
+SOURCE_FILE=$1
 
-if [ $? -ne 0 ]; then
-    echo "Compilation failed"
+# Check if the provided file exists
+if [ ! -f "$SOURCE_FILE" ]; then
+    echo "Error: File $SOURCE_FILE not found."
     exit 1
 fi
 
-echo "compile success"
+# Compile Check
+#gcc -Wall -Werror -o test_file "$SOURCE_FILE"
+gcc -Wall -o test_file "$SOURCE_FILE"
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed for $SOURCE_FILE"
+    exit 1
+fi
+
+echo "Compile success"
 
 # Input and Output Check
-# prepare:
-#    input_i.txt : formal input
-#    text_i.txt  : formal text to read
-#    expect_i.txt: formal result
-# "$?" is a special parameter that expands to the exit status of 
-# the most recently executed foreground pipeline. 
-# It is the exit status of the last command that was executed.
-
 for ((i=1;i<=FILENUM;i++)); do
     echo -e "test $i\n"
     ./test_file <tests/test$i/input_$i.txt > tests/test$i/output_$i.txt
@@ -39,16 +42,10 @@ for ((i=1;i<=FILENUM;i++)); do
         echo "Tests failed for input_$i.txt"
         exit 1
     fi
-    # diff output_$i.txt expected_$i.txt
-    # if [ $? -ne 0 ]; then
-    #     echo "Output does not match expected output for input_$i.txt"
-    #     exit 1
-    # fi
     cat tests/test$i/output_$i.txt
     cat tests/test$i/expected_$i.txt
     echo -e "\n"
 done
 
-# echo "All tests passed"
-echo "all tests done"
+echo "All tests done"
 exit 0
